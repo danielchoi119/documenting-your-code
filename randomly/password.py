1778f1fcb1b84f0e743dec43866c0068a0815fec
Seunghwan Choi <seunghwan.choi1@sony.com>
2022-04-28 13:42:50 -0700

    Adding docs

--- randomly/password.py
+++ randomly/password.py
@@ -1,17 +1,34 @@
 import random
 import string
 
+from typing import Iterable, Optional
 
-def generate_password(chars, punctuation, invalid_chars=None):
+
+def generate_password(chars: int, punctuation: bool, invalid_chars: Optional[Iterable[str]]=None) -> str:
+    """
+    Generates a password randomly.
+
+    Args:
+        chars (int):  number of characters you want in the password
+        punctuation (bool): True if you want to include the punctuation
+        invalid_chars (Optional[Iterable[str]]): characters that you do not want in the password. Defaults to None
+
+    Returns:
+        str: a random password with length = chars
+    """
     valid_chars = string.ascii_letters + string.digits
 
     if punctuation:
         valid_chars += string.punctuation
 
+    # for each invalid character, we will replace it with an empty string to remove that character.
     for invalid_char in invalid_chars:
         valid_chars = valid_chars.replace(invalid_char, "")
 
+    # choose k number of characters randomly from valid_chars
     password_chars = random.choices(valid_chars, k=chars)
+
+    # since password_chars is a list, we will join it using generator comprehension.
     password = "".join(char for char in password_chars)
 
     return password
